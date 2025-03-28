413750a9d2e728075c7da87527617d9e44bd6e90
nitishxyz <nitishxyz@proton.me>
2025-03-28 22:31:20 +0530

    vpc id updated for stages

--- infra/vpc.ts
+++ infra/vpc.ts
@@ -1,4 +1,4 @@
 export const vpc =
   $app.stage === "prod" || $app.stage === "dev"
     ? new sst.aws.Vpc("chainhookVpc", { bastion: true, nat: "ec2" })
-    : sst.aws.Vpc.get("chainhookVpc", "vpc-04a16eef27b7b30e0");
+    : sst.aws.Vpc.get("chainhookVpc", "vpc-0f1d148e6e44b7925");
